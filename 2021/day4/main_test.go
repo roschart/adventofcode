d954d12e02fe5cd3e5f0412c01e2d9424f326f90
jl.balirac <jl.balirac@payvision.com>
2021-12-13 18:37:29 +0100

    solved day4

--- 2021/day4/main_test.go
+++ 2021/day4/main_test.go
@@ -11,6 +11,84 @@ import (
 
 type board = []int
 
+func secondStar(filename string) int {
+	var lastwinner board
+	var lastNumber int
+	numbers, boards := parserFile(filename)
+	for _, n := range numbers {
+		bs := boards[:]
+		boards = nil
+		for i := 0; i < len(bs); i++ {
+			b := bs[i]
+			wins := mark(b, n)
+			if wins {
+				lastwinner = b
+				lastNumber = n
+			} else {
+				boards = append(boards, b)
+			}
+		}
+	}
+
+	return puntuation(lastwinner, lastNumber)
+}
+
+func firstStar(filename string) int {
+	numbers, boards := parserFile(filename)
+	for _, n := range numbers {
+		for _, b := range boards {
+			wins := mark(b, n)
+			if wins {
+				return puntuation(b, n)
+			}
+		}
+	}
+	return -1
+}
+
+func puntuation(b board, n int) int {
+	sum := 0
+	for _, v := range b {
+		if v != -1 {
+			sum += v
+		}
+	}
+	return sum * n
+}
+
+func mark(b board, n int) bool {
+	for i, v := range b {
+		if v == n {
+			b[i] = -1
+			if isRow(b, i) || isColumn(b, i) {
+				return true
+			}
+			break
+		}
+	}
+	return false
+}
+
+func isColumn(b board, pos int) bool {
+	col := pos % 5
+	for i := 0; i < 5; i++ {
+		if b[i*5+col] != -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func isRow(b []int, pos int) bool {
+	row := pos / 5
+	for i := 0; i < 5; i++ {
+		if b[row*5+i] != -1 {
+			return false
+		}
+	}
+	return true
+}
+
 func parserFile(filename string) (numbers []int, boards []board) {
 	file, err := os.Open(filename)
 
@@ -68,8 +146,47 @@ func readNumbers(s string) (numbers []int) {
 	return
 }
 
-func TestExampe(t *testing.T) {
-	numbers, boards := parserFile("example")
-	t.Log(numbers, boards)
-	t.Fail()
+func TestExample(t *testing.T) {
+	result := firstStar("example")
+	expected := 4512
+
+	if expected != result {
+		t.Errorf("Expected %d,  got %d", expected, result)
+	}
+}
+
+func TestCollumn(t *testing.T) {
+	result := firstStar("example2")
+	expected := 2450
+
+	if expected != result {
+		t.Errorf("Expected %d,  got %d", expected, result)
+	}
+}
+
+func TestFirstStar(t *testing.T) {
+	result := firstStar("input")
+	expected := 64084
+
+	if expected != result {
+		t.Errorf("Expected %d,  got %d", expected, result)
+	}
+}
+
+func TestExampleSecondStar(t *testing.T) {
+	result := secondStar("example")
+	expected := 1924
+
+	if expected != result {
+		t.Errorf("Expected %d,  got %d", expected, result)
+	}
+}
+
+func TestSecondtStar(t *testing.T) {
+	result := secondStar("input")
+	expected := 64084
+
+	if expected != result {
+		t.Errorf("Expected %d,  got %d", expected, result)
+	}
 }
