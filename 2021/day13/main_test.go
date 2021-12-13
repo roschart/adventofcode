b18aa9f41b76ae58387df90b2ffc70fba7c7a6de
jl.balirac <jl.balirac@payvision.com>
2021-12-13 16:09:10 +0100

    solved day13

--- 2021/day13/main_test.go
+++ 2021/day13/main_test.go
@@ -16,19 +16,19 @@ const (
 	Y
 )
 
-type dot struct {
+type Dot struct {
 	X int
 	Y int
 }
 
-type fold struct {
+type Fold struct {
 	axe   axe
 	value int
 }
 
 func firstStar(filename string) int {
-	var dots []dot
-	var folds []fold
+	var dots []Dot
+	var folds []Fold
 	dots, folds = parseFile(filename)
 	dots = makeFold(dots, folds[0])
 	solution := countPoints(dots)
@@ -36,15 +36,81 @@ func firstStar(filename string) int {
 
 }
 
-func countPoints(dots []dot) int {
-	panic("unimplemented")
+func secondStar(filename string, nx int, ny int) {
+	var dots []Dot
+	var folds []Fold
+	dots, folds = parseFile(filename)
+	for _, f := range folds {
+		dots = makeFold(dots, f)
+	}
+	printDots(dots, nx, ny)
+
 }
 
-func makeFold(dots []dot, fold fold) []dot {
-	panic("unimplemented")
+func printDots(dots []Dot, nx int, ny int) {
+	matrix := make([][]rune, nx)
+	for i := range matrix {
+		matrix[i] = make([]rune, ny)
+		for j := range matrix[i] {
+			matrix[i][j] = ' '
+		}
+	}
+
+	for _, d := range dots {
+		matrix[d.Y][d.X] = 'X'
+	}
+
+	for i := 0; i < nx; i++ {
+		fmt.Println(string(matrix[i]))
+	}
 }
 
-func parseFile(filename string) (dots []dot, folds []fold) {
+func countPoints(dots []Dot) int {
+	return len(dots)
+}
+
+func makeFold(dots []Dot, fold Fold) []Dot {
+	var result []Dot
+	if fold.axe == Y {
+		for _, d := range dots {
+			if d.Y < fold.value {
+				result = append(result, d)
+			} else if d.Y > fold.value {
+				// y1= 2f-y+1
+				y1 := 2*fold.value - d.Y
+				result = append(result, Dot{X: d.X, Y: y1})
+			}
+		}
+	} else {
+		for _, d := range dots {
+			if d.X < fold.value {
+				result = append(result, d)
+			} else if d.X > fold.value {
+				// x1= 2f-x+1
+				x1 := 2*fold.value - d.X
+				result = append(result, Dot{X: x1, Y: d.Y})
+			}
+		}
+	}
+	return deduplicate(result)
+}
+
+func deduplicate(dots []Dot) []Dot {
+	const N = 10000
+	m := make(map[int]Dot)
+	for _, d := range dots {
+		x := d.X*N + d.Y
+		m[x] = d
+	}
+	var result []Dot
+	for _, d := range m {
+		result = append(result, d)
+	}
+
+	return result
+}
+
+func parseFile(filename string) (dots []Dot, folds []Fold) {
 	state := "reading_dots"
 	file, err := os.Open(filename)
 	if err != nil {
@@ -71,27 +137,27 @@ func parseFile(filename string) (dots []dot, folds []fold) {
 	return dots, folds
 }
 
-func readDot(s string) dot {
+func readDot(s string) Dot {
 	ns := strings.Split(s, ",")
 	x, _ := strconv.Atoi(ns[0])
 	y, _ := strconv.Atoi(ns[1])
-	return dot{
+	return Dot{
 		X: x,
 		Y: y,
 	}
 }
 
-func readFold(s string) fold {
+func readFold(s string) Fold {
 	ns := strings.Split(s, "=")
 	value, _ := strconv.Atoi(ns[1])
 	dir := ns[0][len(ns[0])-1]
 	if dir == 'y' {
-		return fold{
+		return Fold{
 			axe:   Y,
 			value: value,
 		}
 	} else {
-		return fold{
+		return Fold{
 			axe:   X,
 			value: value,
 		}
@@ -100,11 +166,24 @@ func readFold(s string) fold {
 
 }
 
-func TestExampe(t *testing.T) {
+func TestExample(t *testing.T) {
 	got := firstStar("example")
 	expected := 17
 	if expected != got {
 		t.Errorf("Expected %d,  got %d", expected, got)
 	}
+}
+
+func TestFirstStar(t *testing.T) {
+	got := firstStar("input")
+	expected := 724
+	if expected != got {
+		t.Errorf("Expected %d,  got %d", expected, got)
+	}
+}
+
+func TestSecondStar(t *testing.T) {
+	secondStar("input", 6, 40)
+	t.Fail()
 
 }
