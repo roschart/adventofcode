6de2eb290cd82595c7da7d694099ca878fec614d
jl.balirac <jl.balirac@payvision.com>
2021-12-16 09:37:29 +0100

    Refactor extrad neigbors to funtion

--- 2021/day09/main_test.go
+++ 2021/day09/main_test.go
@@ -10,13 +10,29 @@ import (
 
 type Cave [][]int
 
-type Point struct {
+type Coord struct {
 	X, Y int
 }
 
-type LowPoints map[Point]int
+type Points map[Coord]int
 
-func solution(filename string) int {
+func solutionBasin(filename string) int {
+	cave := parseFile(filename)
+	lowPoints := getLowPoints(cave)
+	basins := getBasins(cave, lowPoints)
+	sum := 0
+	for _, v := range basins {
+		sum += v + 1
+	}
+	return sum
+}
+
+func getBasins(cave Cave, lowPoints Points) []int {
+
+	panic("Pending to implement")
+}
+
+func solutionLowPoints(filename string) int {
 	cave := parseFile(filename)
 	lowPoints := getLowPoints(cave)
 	sum := 0
@@ -26,34 +42,41 @@ func solution(filename string) int {
 	return sum
 }
 
-func getLowPoints(cave Cave) (lowPoints LowPoints) {
-	lowPoints = make(LowPoints)
-	neibors := [][]int{
+func getLowPoints(cave Cave) (lowPoints Points) {
+	lowPoints = make(Points)
+	for y, row := range cave {
+		for x, v := range row {
+			ismin := true
+			for _, v1 := range getNeigbors(cave, x, y) {
+				if v1 <= v {
+					ismin = false
+					break
+				}
+			}
+			if ismin {
+				lowPoints[Coord{x, y}] = v
+			}
+		}
+	}
+	return lowPoints
+}
+
+func getNeigbors(cave Cave, x, y int) (neigbors Points) {
+	neigbors = make(Points)
+	near := [][]int{
 		{-1, 0},
 		{1, 0},
 		{0, -1},
 		{0, 1},
 	}
-	for y, row := range cave {
-		for x, v := range row {
-			ismin := true
-			for _, cn := range neibors {
-				j := y + cn[0]
-				i := x + cn[1]
-				if i >= 0 && j >= 0 && i < len(cave[0]) && j < len(cave) {
-					v1 := cave[j][i]
-					if v1 <= v {
-						ismin = false
-						break
-					}
-				}
-			}
-			if ismin {
-				lowPoints[Point{x, y}] = v
-			}
+	for _, n := range near {
+		j := y + n[0]
+		i := x + n[1]
+		if i >= 0 && j >= 0 && i < len(cave[0]) && j < len(cave) {
+			neigbors[Coord{i, j}] = cave[j][i]
 		}
 	}
-	return lowPoints
+	return neigbors
 }
 
 func parseFile(filename string) (cave Cave) {
@@ -84,7 +107,7 @@ func TestSolution(t *testing.T) {
 		{"input", 425},
 	}
 	for _, c := range cases {
-		got := solution(c.filename)
+		got := solutionLowPoints(c.filename)
 		expected := c.result
 		if expected != got {
 			t.Errorf("Expected %d,  got %d", expected, got)
