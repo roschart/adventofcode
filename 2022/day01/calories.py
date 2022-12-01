649b3e7f4343b87f6246e1c0ba7e8b1f038d5d45
roschart <jl.balirac@hotmail.com>
2022-12-01 09:29:28 +0100

    solved second puzzle

--- 2022/day01/calories.py
+++ 2022/day01/calories.py
@@ -1,16 +1,18 @@
-import os
-import pathlib
-
 with open('input', mode ='r') as file:
+    calories:list[int]=[]
     max_calories:int=0
     lines = file.readlines()
     current_elf_calories :int=0
     for line in lines:
         num=line.strip()
         if (num==""):
-            if(current_elf_calories>max_calories):
-                max_calories=current_elf_calories
+            calories.append(current_elf_calories)
             current_elf_calories=0
         else:
             current_elf_calories+=int(num)
-print(max_calories)
+calories.sort()
+top3=calories[-3:]
+print("Max=", max(calories))
+print("top3", top3)
+print("Last=", top3[2])
+print("Total=", sum(top3))
