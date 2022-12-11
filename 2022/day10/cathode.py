08d13480a152c8ac34a9abd7a6e3c8871fcdc576
roschart <jl.balirac@hotmail.com>
2022-12-11 10:17:42 +0100

    Solved day10

--- 2022/day10/cathode.py
+++ 2022/day10/cathode.py
@@ -1,42 +1,68 @@
 from enum import Enum
 
+
 class State(Enum):
     FREE = 0
     ADDING = 1
 
-def execute( command:str) -> State:
-    if command=='noop':
+
+def execute(command: str) -> State:
+    if command == 'noop':
         return State.FREE
     else:
         return State.ADDING
-    
-def app(filename:str)->int:
+
+
+def app(filename: str) -> tuple[int, list[str]]:
     with open(filename, mode='r') as file:
-        commands=[line.strip() for line in file.readlines()]
-        x:int=1
-        checks:list[int]=[20,60,100, 140, 180, 220]
-        strength:list[int]=[]
-        state:State =State.FREE
-        command="noop"
-        for i in range(1,221):
+        commands = [line.strip() for line in file.readlines()]
+        x: int = 1
+        checks: list[int] = [20, 60, 100, 140, 180, 220]
+        strength: list[int] = []
+        state: State = State.FREE
+        command = "noop"
+        pixels: list[str] = []
+        i=1
+        while len(commands)>0:
+            p = (i-1)%40
+            if (abs(p-x) < 2):
+                pixels.append("#")
+            else:
+                pixels.append(".")
+
             if i in checks:
                 strength.append(x*i)
-            if state==State.FREE:
-                command=commands.pop(0)
-                state=execute(command)
-            elif state==State.ADDING:
-                state=State.FREE
-                x+=int(command.split()[1])              
-                 
-            print(f"Cicle {i}, status {state}, lastCommand= {command} \t x={x}")
-        print(f"Strengz\n{strength}\n----")
-        return sum(strength)
-            
 
-if __name__== "__main__":
-    e=app("day10/example")
-    if e!=13140:
+            if state == State.FREE:
+                command = commands.pop(0)
+                state = execute(command)
+            elif state == State.ADDING:
+                state = State.FREE
+                x += int(command.split()[1])
+            i+=1
+        return sum(strength), pixels
+
+def print_crd(pixesl:list[str], replace:bool=False):
+    s="".join(pixesl)
+    if replace:
+        s=s.replace(".", " ")
+        
+    print(s[0:40])
+    print(s[40:80])
+    print(s[80:120])
+    print(s[120:160])
+    print(s[160:200])
+    print(s[200:240])
+
+if __name__ == "__main__":
+    e, ps = app("day10/example")
+    if e != 13140:
         raise ValueError(f"Sinal streng of {e} no correct for the example")
-    i=app("day10/input")
-    print(f"El valor es {i}")
+    print_crd(ps)
+    if "".join(ps)!= "##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......###########.......#######.......#######.....":
+        raise ValueError("Se ha pintado mal el ejemplo")
     
+    i,ps = app("day10/input")
+    print(f"El valor es {i}")
+    print("------")
+    print_crd(ps,True)
