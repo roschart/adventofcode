3482394cd36d013421b81d6b2895ca751253885f
roschart <jl.balirac@hotmail.com>
2022-12-11 23:53:14 +0100

    solved day11

--- 2022/day11/keep_away.py
+++ 2022/day11/keep_away.py
@@ -27,30 +27,22 @@ class Monkey:
         if_false =int(lines[5].split(" ")[-1])
         return Monkey(id,items,operation,test,if_true,if_false)
     
-        
-    
+
+           
 Monkeys= dict[int,Monkey]
 
 def round(monkesy:Monkeys):
     for id in range(len(monkesy)):
         m=monkesy[id]
-        print (f"Monkey {id}:")
         for i in range(len(m.items)):
             e=m.items.pop(0)
             m.inspected+=1
-            print(f"  Monkey inspects an item with a worry level of {e}.")
             e=m.opertion(e)
-            print(f"    Worry level is {e}.")
             e=e//3
-            print(f"    Monkey gets bored with item. Worry level is divided by 3 to {e}")
             div= e % m.test
             if div!=0:
-                print(f"    Current worry level is not divisible by {m.test}.")
-                print(f"    Item with worry level {e} is thrown to monkey {m.if_false}")
                 monkesy[m.if_false].items.append(e)
             else:
-                print(f"    Current worry level is divisible by {m.test}.")
-                print(f"    Item with worry level {e} is thrown to monkey {m.if_true}")
                 monkesy[m.if_true].items.append(e)
             
 def app(filename:str)->int:
@@ -61,21 +53,111 @@ def app(filename:str)->int:
     for i in range((len(lines)+1)//7):
         m=Monkey.from_lines(lines[i*7:i*7+7])
         monkeys[m.id]=m
+    xs:list[int]=[]
     for i in range(20):
+        xs=[]
         round(monkeys)
-        print(f"After Round {i+1}")
-        for id in range(len(monkeys)):
-            print(f"Monkey {id}: {monkeys[id].items}")
-    xs=[]
+    print(f"After Round {i+1}")
     for id in range(len(monkeys)):
         xs.append(monkeys[id].inspected)
         print(f"Monkey {id} inspected items {monkeys[id].inspected} times.")
     xs.sort()
     return xs[-2]*xs[-1]
 
+
+
+
+@dataclass
+class Item:
+    values:dict[int,int]
+    
+    @staticmethod
+    def from_str(s:str)->'Item':
+        i=int(s)
+        primes=[2,3,5,7, 11, 13,17,19,23]
+        v={p:i%p for  p in primes}
+        return Item(v)
+    def add(self, x:int)->'Item':
+        return Item({k:(v +x)%k for k, v in self.values.items()})
+    def mul(self, x:int)->'Item':
+        return Item({k:(v * x)%k for k, v in self.values.items()})
+    def square (self )->'Item':
+        return Item({k:(v * v)%k for k, v in self.values.items()})
+    def is_divisible_by(self,v:int)->bool:
+        return self.values[v]==0
+         
+
+@dataclass
+class Monkey2:
+    id: int
+    items: list[Item]
+    opertion: str
+    test: int
+    if_true: int
+    if_false: int
+    inspected: int = 0
+    
+    @staticmethod
+    def from_lines(lines:list[str])->'Monkey2':
+        id=int(lines[0][7])
+        items= [Item.from_str(x) for x in lines[1][16:].split(", ")]
+        operation=lines[2]
+        test =int(lines[3].split(" ")[-1])
+        if_true =int(lines[4].split(" ")[-1])
+        if_false =int(lines[5].split(" ")[-1])
+        return Monkey2(id,items,operation,test,if_true,if_false)
+
+Monkeys2= dict[int,Monkey2]
+
+def round2(monkeys2:Monkeys2):
+    for id in range(len(monkeys2)):
+        m=monkeys2[id]
+        for j in range(len(m.items)):
+            i=m.items.pop(0)
+            m.inspected+=1
+            if "+" in m.opertion:
+                x =int(m.opertion.split(" ")[-1])
+                i= i.add(x)
+            elif "old * old" in m.opertion:
+                i=i.square() 
+            elif "*" in m.opertion:
+                x =int(m.opertion.split(" ")[-1])
+                i= i.mul(x)
+            else:
+                raise ValueError(f"No valid operation {m.opertion}")
+            
+            if i.is_divisible_by(m.test):
+                monkeys2[m.if_true].items.append(i)
+            else:
+                monkeys2[m.if_false].items.append(i)    
+    
+def app2(filename:str)->int:
+    lines:list[str]=[]
+    monkeys2:Monkeys2=dict()
+    with open(filename, mode='r') as file:
+        lines=[l.strip() for l in file.readlines()]
+    for i in range((len(lines)+1)//7):
+        m=Monkey2.from_lines(lines[i*7:i*7+7])
+        monkeys2[m.id]=m
+    for i in range(10000):
+        round2(monkeys2)
+    xs=[]
+    for id in range(len(monkeys2)):
+        xs.append(monkeys2[id].inspected)
+        print(f"Monkey {id} inspected items {monkeys2[id].inspected} times.")
+    xs.sort()
+    return xs[-2]*xs[-1]
+
+    
 if __name__=="__main__":
     r=app("day11/example")
     if r!=10605:
         raise ValueError(f"Bad response {r} for the example")
+    r=app2("day11/example")
+    if r!=2713310158:
+        raise ValueError(f"Bad response {r} for the example 2")    
     r=app("day11/input")
     print(F"El valor del puzzle 1 es {r}")
+    r=app2("day11/input")
+    print(F"El valor del puzzle 2 es {r}")
+    
