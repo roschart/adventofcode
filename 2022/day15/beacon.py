2ece2ce69c52bb56e684e9ece5fa1f015c9359fc
roschart <jl.balirac@hotmail.com>
2022-12-16 15:34:45 +0100

    refact: for optimization

--- 2022/day15/beacon.py
+++ 2022/day15/beacon.py
@@ -24,13 +24,36 @@ def distance(a: Point, b: Point) -> int:
 
 
 def app(filiename: str,row:int) -> int:
+    pairs = get_pairs_from_file(filiename)
+    left = min([p.sesor.x-p.radio() for p in pairs])
+    right = max([c.sesor.x+c.radio() for c in pairs])
+    return not_present_row(y=row, left=left, right=right, pairs=pairs)
+
+def app2(filiename: str, n:int) -> int:
+    pairs = get_pairs_from_file(filiename)
+    for y in range(n+1):
+        x=0
+        while x<=n:
+            e,next= in_radius_of_some(Point(x,y),pairs)
+            if e:
+                x=next
+            else:
+                return x*4000000+y
+            x+=1
+        
+    for y in range(n+1):
+        for x in range(n+1):
+            if not in_radius_of_some(Point(x,y),pairs):
+                return x*4000000+y
+        print(f"Linea {y} procesada")
+    raise ValueError("app2 without solution")
+
+def get_pairs_from_file(filiename)->list[Pair]:
     lines: list[str] = []
     with open(filiename, mode='r') as file:
         lines = [l.strip() for l in file.readlines()]
     pairs = generate_pairs(lines)
-    left = min([p.sesor.x-p.radio() for p in pairs])
-    right = max([c.sesor.x+c.radio() for c in pairs])
-    return not_present_row(y=row, left=left, right=right, pairs=pairs)
+    return pairs
 
 
 def not_present_row(y: int, left: int, right: int, pairs: list[Pair]) -> int:
@@ -42,17 +65,19 @@ def not_present_row(y: int, left: int, right: int, pairs: list[Pair]) -> int:
             continue
         if Point(x,y) in beacons:
             continue
-        if in_radius_of_some(Point(x, y), pairs):
+        b,_=in_radius_of_some(Point(x, y), pairs)
+        if b:
             count += 1
     return count
 
 
-def in_radius_of_some(p: Point, pairs: list[Pair]) -> bool:
+def in_radius_of_some(p: Point, pairs: list[Pair]) -> tuple[bool,int]:
 
     for c in pairs:
         if distance(p, c.sesor) <= c.radio():
-            return True
-    return False
+            d=p.x-c.sesor.x
+            return True, c.sesor.x+d
+    return False, 0
 
 
 def generate_pairs(lines: list[str]) -> list[Pair]:
@@ -73,4 +98,11 @@ if __name__ == "__main__":
     r = app("day15/input",2000000)
     if r != 6124805:
         raise ValueError(f"Value {r} nod valid for example puzzle 1")
-    print("hello")
+    
+    # r=app2("day15/example",20)
+    # if r!=56000011:
+    #     raise ValueError(f"Value {r} nod valid for example puzzle 2")
+    # r=app2("day15/input",4000000)
+    # if r!=0:
+    #     raise ValueError(f"Value {r} nod valid for example puzzle 2")
+    # print("hello")
