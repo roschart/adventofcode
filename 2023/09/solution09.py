f1f0fcea1c37a281b0c755843d369e485cef591c
roschart <jl.balirac@hotmail.com>
2023-12-10 01:05:45 +0000

    refactor, extract methods

--- 2023/09/solution09.py
+++ 2023/09/solution09.py
@@ -1,8 +1,8 @@
 import re
-from typing import List
+from typing import Iterator, List
 
 
-def extrapolate(secuences: List[List[int]]) -> int:
+def nexts(secuences: List[List[int]]) -> int:
     n = 0
     for s in secuences[-2::-1]:
         a = s[-1]
@@ -10,27 +10,27 @@ def extrapolate(secuences: List[List[int]]) -> int:
     return n
 
 
-def nexts(repots: List[List[int]]) -> List[int]:
-    nexts: List[int] = []
+def generate(repots: List[List[int]]) -> Iterator[List[List[int]]]:
     for secuence in reports:
         secuences: List[List[int]] = [secuence]
         while not all(e == 0 for e in secuence):
             secuence = [b-a for a, b in zip(secuence, secuence[1:])]
             secuences.append(secuence)
-
-        nexts.append(extrapolate(secuences))
-    return nexts
+        yield secuences
 
 
 example = "09/example"
+input = "09/input"
 file = example
 row_data = [lines.strip() for lines in open(file)]
 
 reports = [[int(num) for num in re.findall(r"-?\d+", line)]
            for line in row_data]
 
-s = sum(nexts(reports))
+
+s = sum(nexts(s) for s in generate(reports))
 if file == example and s != 114:
     raise Exception
 
-print(s)
+if file == input and s != 1993300041:
+    raise Exception
