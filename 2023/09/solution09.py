53ed73a2f7049b26b189924a56a3557ed1f85ce1
roschart <jl.balirac@hotmail.com>
2023-12-10 01:45:14 +0000

    solved 09/2

--- 2023/09/solution09.py
+++ 2023/09/solution09.py
@@ -2,11 +2,19 @@ import re
 from typing import Iterator, List
 
 
-def nexts(secuences: List[List[int]]) -> int:
+def next(secuences: List[List[int]]) -> int:
     n = 0
     for s in secuences[-2::-1]:
         a = s[-1]
         n += a
+    return n
+
+
+def previous(secuences: List[List[int]]) -> int:
+    n = 0
+    for s in secuences[-2::-1]:
+        a = s[0]
+        n = a - n
     return n
 
 
@@ -21,16 +29,23 @@ def generate(repots: List[List[int]]) -> Iterator[List[List[int]]]:
 
 example = "09/example"
 input = "09/input"
-file = example
+file = input
 row_data = [lines.strip() for lines in open(file)]
 
 reports = [[int(num) for num in re.findall(r"-?\d+", line)]
            for line in row_data]
 
-
-s = sum(nexts(s) for s in generate(reports))
-if file == example and s != 114:
+history = list(generate(reports))
+n = sum(next(s) for s in history)
+p = sum(previous(s) for s in history)
+if file == example and n != 114:
     raise Exception
 
-if file == input and s != 1993300041:
-    raise Exception
+if file == example and p != 2:
+    raise Exception(f"{p}")
+
+if file == input and n != 1993300041:
+    raise Exception(f"{n}")
+
+if file == input and p != 1038:
+    raise Exception(f"{p}")
