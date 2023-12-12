7c30286dec8c79be37f442099d4f546e3148d925
roschart <jl.balirac@hotmail.com>
2023-12-12 08:18:46 +0000

    solved 11/01

---
+++ 2023/11/solution11.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+from typing import Dict, List, Set, Tuple
+
+
+@dataclass(frozen=True)
+class Coord:
+    r: int
+    c: int
+
+
+def get_galaxies(data: List[str]) -> Set[Coord]:
+    galaxies: Set[Coord] = set()
+    for i, line in enumerate(data):
+        for j, v in enumerate(line):
+            if v == "#":
+                galaxies.add(Coord(i, j))
+    return galaxies
+
+
+def distance(pair: Tuple[Coord, Coord]) -> int:
+    a, b = pair
+    d = abs(a.r-b.r)+abs(a.c-b.c)
+    return d
+
+
+def distances(file: str) -> List[int]:
+    data = [line.strip() for line in open(file)]
+
+    rows = len(data)
+    cols = len(data[0])
+
+    galaxies = get_galaxies(data)
+
+    rows_to_expand = set(range(rows))
+    cols_to_expand = set(range(cols))
+
+    for c in galaxies:
+        rows_to_expand.discard(c.r)
+        cols_to_expand.discard(c.c)
+
+
+# Expand rows
+    expadended: List[str] = []
+    for i, row in enumerate(data):
+        expadended.append(row)
+        if i in rows_to_expand:
+            expadended.append("."*cols)
+
+# Expand cols
+    for r, row in enumerate(expadended):
+        result = []
+        for i, char in enumerate(row):
+            result.append(char)
+            if i in cols_to_expand:
+                result.append(".")
+        expadended[r] = ''.join(result)
+
+    galaxies = get_galaxies(expadended)
+
+    pairs: Set[Tuple[Coord, Coord]] = set()
+    galaxies_copy = galaxies.copy()
+    a = galaxies_copy.pop()
+    while (len(galaxies_copy) > 0):
+        for b in galaxies_copy:
+            pairs.add((a, b))
+        a = galaxies_copy.pop()
+
+    # Checks
+    if file == "11/example":
+        one = Coord(0, 4)
+        seven = Coord(10, 9)
+        three = Coord(2, 0)
+        six = Coord(7, 12)
+        eight = Coord(11, 0)
+        nine = Coord(11, 5)
+
+        cs = [one, seven, three, six, eight, nine]
+        for c in cs:
+            if c not in galaxies:
+                print(galaxies)
+                raise Exception(f"{c}")
+
+        d: Dict[Tuple[Coord, Coord], int] = {
+            (one, seven): 15, (three, six): 17, (eight, nine): 5}
+        for k, v in d.items():
+            if distance(k) != v:
+                raise Exception(f"{k},{v}")
+
+    ds: List[int] = list()
+    for p in pairs:
+        ds.append(distance(p))
+    return ds
+
+
+file = "11/example"
+s = sum(distances(file))
+if s != 374:
+    raise Exception(f"for {file}:{s}")
+
+file = "11/input"
+s = sum(distances(file))
+if s != 0:
+    raise Exception(f"for {file}:{s}")
