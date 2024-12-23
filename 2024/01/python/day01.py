579b8208663b974a247868db820b04e796c632c4
Roschart <jl.balirac@hotmail.com>
2024-12-23 21:47:52 +0000

    Add test and solved day1 part 1

--- 2024/01/python/day01.py
+++ 2024/01/python/day01.py
@@ -1,4 +1,7 @@
-def process_line(line: str) -> tuple[int, int]:
+import unittest
+from typing import Iterator, Tuple, List
+
+def process_line(line: str) -> Tuple[int, int]:
     """
     Parses a line and extracts two integers.
     
@@ -15,17 +18,35 @@ def process_line(line: str) -> tuple[int, int]:
     parts = stripped_line.split()
     if len(parts) != 2:
         raise ValueError(f"Invalid line format: '{line}'")
-    
     try:
         return tuple(map(int, parts))
     except ValueError as e:
         raise ValueError(f"Error parsing line: '{line}' - {e}")
 
-# Example usage:
-with open("../input", "r") as f:
-    for line in f:
-        try:
-            result = process_line(line)
-            print(result)
-        except ValueError as e:
-            print(e)
+def get_data_from_file(file_path: str) -> Tuple[List[int], List[int]]:
+    col1, col2 = [], []
+    with open(file_path, "r") as f:
+        for line in f:
+            num1, num2 = process_line(line)
+            col1.append(num1)
+            col2.append(num2)
+    return col1, col2
+
+def calculate_distance(col1: list[int], col2: list[int]) -> int:
+    col1.sort()
+    col2.sort()
+    return sum(abs(a - b) for a, b in zip(col1, col2))
+
+
+
+
+
+if __name__ == "__main__":
+    file_path = "../input"  # Replace with your file path
+    try:
+        col1, col2 = get_data_from_file(file_path)
+        result=calculate_distance(col1,col2)
+        print("Result: ", result)
+    except ValueError as e:
+        print("Error:", e)
+
---
+++ 2024/01/python/test_day.py
@@ -0,0 +1,20 @@
+import unittest
+
+from day01 import get_data_from_file, calculate_distance
+
+class TestCalculateResult(unittest.TestCase):
+    def test_example(self):
+        file_path = "../test" 
+        expected_result = 11
+
+        # Read and process the test file
+        col1, col2 = get_data_from_file(file_path)
+
+        # Calculate the result
+        result = calculate_distance(col1, col2)
+
+        # Assert the expected result
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
